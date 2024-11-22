a19a1ee6b4a36f9843aa5ccebda2aced7a6babca
gagaswin <gagaswahyuin@gmail.com>
2024-11-22 15:34:48 +0700

    initalize git

---
+++ src/test/java/com/gagaswin/silent_meeting/SilentMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagaswin.silent_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilentMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
