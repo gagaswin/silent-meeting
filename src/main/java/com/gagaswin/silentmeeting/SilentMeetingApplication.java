a64c7b2c1391fbccd1caca839be2372cc28c7517
gagaswin <gagaswahyuin@gmail.com>
2024-11-23 17:32:10 +0700

    fix: rename package name

--- src/main/java/com/gagaswin/silentmeeting/SilentMeetingApplication.java
+++ src/main/java/com/gagaswin/silentmeeting/SilentMeetingApplication.java
@@ -1,4 +1,4 @@
-package com.gagaswin.silent_meeting;
+package com.gagaswin.silentmeeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gagaswin/silentmeeting/SilentMeetingApplicationTests.java
+++ src/test/java/com/gagaswin/silentmeeting/SilentMeetingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gagaswin.silent_meeting;
+package com.gagaswin.silentmeeting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
