9befa0bff3a09777ccd2d4b4cfb19c763e979ccf
gagaswin <gagaswahyuin@gmail.com>
2024-11-26 16:37:31 +0700

    fix: entity user and user detail

--- src/main/java/com/gagaswin/silentmeeting/modules/users/model/entity/User.java
+++ src/main/java/com/gagaswin/silentmeeting/modules/users/model/entity/User.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -23,19 +24,28 @@ public class User {
   @Column(name = "id")
   private String id;
 
-  @Column(name = "name", nullable = false, length = 100)
-  private String name;
+  @Column(name = "username", nullable = false, length = 100, unique = true)
+  private String username;
 
   @Column(name = "email", nullable = false, length = 320, unique = true)
   private String email;
 
+  @Column(name = "first_name", nullable = false, length = 100)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false, length = 100)
+  private String lastName;
+
   @Column(name = "password", nullable = false, length = 100)
   private String password;
 
+  @Column(name = "created_at")
+  private LocalDateTime createdAt = LocalDateTime.now();
+
+//  RELATION
   @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
   private UserDetail userDetail;
 
-//  RELATION
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "user_role",
       joinColumns = @JoinColumn(name = "user_id"),
--- src/main/java/com/gagaswin/silentmeeting/modules/users/model/entity/UserDetail.java
+++ src/main/java/com/gagaswin/silentmeeting/modules/users/model/entity/UserDetail.java
@@ -1,10 +1,13 @@
 package com.gagaswin.silentmeeting.modules.users.model.entity;
 
+import com.gagaswin.silentmeeting.common.constant.ELastEducation;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -20,8 +23,18 @@ public class UserDetail {
   @Column(name = "phone", length = 15)
   private String phone;
 
+  @Column(name = "date_of_birth")
+  private LocalDate dateOfBirth;
+
   @Column(name = "address", length = 150)
   private String address;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "last_education")
+  private ELastEducation lastEducation;
+
+  @Column(name = "last_institution_name", length = 100)
+  private String lastInstitutionName;
 
 //  RELATION
   @OneToOne
